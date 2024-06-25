4e65093c03965ddf2eedfaf069aa0b47d9a4af9f
jeckaGoonz <tatuajes.jessica.g@gmail.com>
2024-06-24 22:41:40 -0400

    Add files via upload

---
+++ ejercicio python/ejercicio.py
@@ -0,0 +1,27 @@
+libros = []
+
+while True:
+
+   
+    print("bienvenidos al inventario y ventas de libros")
+    print("1.elija una opcion ")
+    print("2. registre su libro")
+    print("3.elija la lista de libros")
+    print("4.salir")
+    opcion= int(input("elige una opcion"))
+   
+if opcion==1:
+        fn.registraLibro
+        print(registrodelibros(libros))
+elif opcion==2:
+         fn.registraLibro
+         print() 
+elif opcion==3:
+        fn.listadeLibros
+        print()
+elif opcion==4:
+         fn.salir
+         print("muchas gracias espero regrese pronto")
+
+else:
+      print("ingrese la opcion correcta ")
---
+++ ejercicio python/funciones.py
@@ -0,0 +1,74 @@
+libros = []
+
+while True:
+
+ 
+    print("bienvenidos la venta de libros")
+    print("elija una opcion ")
+    print("2. registre su libro")
+    print("3.elija la lista de libros")
+    print("4.salir")
+    print("---------------------")
+    opcion= int(input("elige una opcion"))
+else:
+    
+   
+   
+if opcion==1:
+        fn.registraLibro
+        print(registrodelibros(libros))
+elif opcion==2:
+         fn.Listadelibro
+         print(Listadolibros(libros)) 
+elif opcion==3:
+        fn.salir
+        print(salir(libro))
+
+        print("muchas gracias espero regrese pronto")
+
+else:
+      print("ingrese la opcion correcta ")
+
+CATEGORIA =["cienciaFiccion" , "noFiccion" , "ciencia"]
+VENTAS =[]
+
+def registrar_Libros(libros):
+    input("escoja los ibros que decee")
+    titulo = input("escoja el titulo del el libro")
+    categoria = input("ingresar la seccion que usted busca : (cienciaFiccion ,noFiccion,ciencia)").lower()
+    while VENTAS not in VENTAS:
+        print("intentar nuevamente")
+valor = int(input("ingresar el precio del libro"))
+bool = 22.000
+def registrar_Ventas(libros):
+        print()
+
+def Imprimir_Reporte_Ventas(libros):
+        print()
+
+def Registrar_lista_Libros(libros):
+        print()
+   
+Libros.append({
+        'nombre',
+        'listas',
+        'ventas',
+        'valor',
+
+    })
+print("ingresa la cantidad de libros que decea")
+   
+   
+nombredearchivo = 'planilla_todos.txt'
+print("si decea continuar precione ENTER ")
+libros in libros 
+with open(nombreArchivo, 'w' ):
+        archivo.write(f"registro de libros:[libro]\n")
+        archivo.write(f"lista de todos los libros:[libro]\n")
+        archivo.write(f"registro de ventas:[libro]\n")
+        archivo.write(f"reporte de ventas:[libro]\n")
+        archivo.write(f"total de libros e inventario:[libro]\n")
+
+                      
+
+
